84daf19b5c2f36056cbed4929ee19dfdac61cfa8
Adil Rasiyani <adil.rasiyani@gmail.com>
2020-06-26 21:53:23 -0500

    initial commit

---
+++ server.ts
@@ -0,0 +1,27 @@
+/* IMPORTS */
+const express = require('express');
+const server = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+const port = process.env.PORT || 5000;
+/* MIDDLEWARE */
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+/* CONNECT TO DATABASE */
+// mongoose.connect(
+//     '',
+//     { useNewUrlParser: true },
+//     (): void => console.log('Connected to db.')
+// );
+
+/* CREATE REQUESTS */
+server.get('/', (req: any, res: any): any => {
+    res.send('HOME');
+});
+
+/* START LISTENING */
+server.listen(port, () => {
+    console.log(`Listening on port ${ port }...`)
+});
