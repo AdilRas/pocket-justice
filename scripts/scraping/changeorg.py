9439441c0d6d7fa5010312301336251a601b4173
Adil Rasiyani <adil.rasiyani@gmail.com>
2020-06-27 15:24:49 -0500

    Add scraping directory

---
+++ scripts/scraping/changeorg.py
@@ -0,0 +1,4 @@
+import requests as req
+from bs4 import BeautifulSoup as BS
+
+res = req.get('https://www.change.org/petitions')
---
+++ scripts/scraping/thepetitionsite.py
@@ -0,0 +1,6 @@
+import requests as req
+from bs4 import BeautifulSoup as BS
+
+res = req.get('https://www.thepetitionsite.com/browse-petitions/#hottest')
+
+
