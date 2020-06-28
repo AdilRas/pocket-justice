90f5ea3780dc9b5809cb1f14ec9c2cecd65f487c
Adil Rasiyani <adil.rasiyani@gmail.com>
2020-06-27 20:50:49 -0500

    Add html headers

--- scripts/scraping/thepetitionsite.py
+++ scripts/scraping/thepetitionsite.py
@@ -3,30 +3,37 @@ from bs4 import BeautifulSoup as BS
 from models.Petition import Petition
 
 BASE_URL = 'https://www.thepetitionsite.com'
-
-res = req.get('https://www.thepetitionsite.com/browse-petitions/#hottest')
+headers = {
+    "User-Agent": "BeautifulSoup"
+}
+res = req.get('https://www.thepetitionsite.com/browse-petitions/', headers = headers)
 src = res.content
 soup = BS(src, 'lxml')
-rawCards = soup.find_all('div', {"class": "petition"})
+# print(soup)
+rawCards = soup.find_all("div", {"class": "petition"})
 
 petitions = []
 
 for card in rawCards:
     title = card.find('div', {"class": "title"}).contents[0]
     desc = None
-    href = BASE_URL + card.attrs['href']
+    href = BASE_URL + card.contents[0].find('a').attrs['href']
 
     #creating new request to access content on each petition's actual page
-    res1 = req.get(href)
+    res1 = req.get(href, headers=headers)
     src1 = res1.content
     soup1 = BS(src1, 'lxml')
-    rawCard1 = soup1.find('div', {"class": "progress hide_from_unsigned progress-completed"})
+    print(soup1)
+    break
+    # rawCard1 = soup1.find(
+    #     'div', {"class": "progress-completed"})
+    # print(rawCard1)
+    # signatures = rawCard1.find("div", {"class": "progress__supporters"}).contents[0]
+    # goal = rawCard1.find("div", {"class": "progress__goal"}).contents[0]
 
-    signatures = rawCard1.find("div", {"class": "progress__supporters"}).contents[0]
-    goal = rawCard1.find("div", {"class": "progress__goal"}).contents[0]
+    # petition = Petition(title=title, description=desc, goal=goal, signatures=signatures, href=href)
+    # petitions.append(petition)
+    # print('Added petition')
 
-    petition = Petition(title=title, description=desc, goal=goal, signatures=signatures, href=href)
-    petitions.append(petition)
-
-print(petitions[0].toString())
+# print(petitions[0].toString())
 
