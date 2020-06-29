b16947e2895bc749793add135ebbd037b973b978
Adil Rasiyani <adil.rasiyani@gmail.com>
2020-06-28 22:53:06 -0500

    update build

---
+++ client/build/precache-manifest.e2e65461e7fd8faba7a2990d71a86412.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a944c6026d70640b065c8df8fbf1ce98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6406fc0726bfb57cf5d",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "ca5bcfbe6a8adb8720e2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6406fc0726bfb57cf5d",
+    "url": "/static/js/2.5231c72e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5231c72e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5bcfbe6a8adb8720e2",
+    "url": "/static/js/main.b433ec9d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
